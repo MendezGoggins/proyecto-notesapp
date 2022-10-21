7e1a8396b2fb90b2e049f1b0bebbeefe0565bfd1
Javi Méndez <xaviermendez88@gmail.com>
2022-10-21 11:17:12 +0200

    version para heroku

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -64,7 +64,8 @@ router.post("/signup", isLoggedOut, (req, res) => {
       return User.create({ username, email, password: hashedPassword });
     })
     .then((user) => {
-      res.redirect("/auth/login");
+      req.session.currentUser = user;
+      res.redirect("/");
     })
     .catch((error) => {
       if (error instanceof mongoose.Error.ValidationError) {
